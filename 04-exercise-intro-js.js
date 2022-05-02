178e2faa1cc94115ec9bb26bc1a453afd51468e8
Zach Rossman <zach.rossman@tech901.org>
2022-05-02 17:08:55 -0500

    js

---
+++ 04-exercise-intro-js.js
@@ -0,0 +1,114 @@
+Guess what answers you would get if you ran this in the Javascript Console in Google Chrome. Once you have an answer to the questions then check them by copying them and running it in the console yourself line by line */ 
+
+// 1. Evaluate each of the following expressions
+
+
+1. 5 + "34" 
+
+2. 5 - "4"
+
+3. 10 % 5
+
+4. 5 % 10
+
+5. "Java" + "Script" 
+
+6. " " + " " 
+
+7. " " + 0 
+
+8. true + true 
+
+9. true + false 
+
+10. false + true 
+
+11. false - true 
+
+12. 3 - 4 
+
+13. "Bob" - "bill" 
+
+
+
+
+ 2. Evaluate each of the following comparisons. What is different about these "comparisons" and the previous "expressions?"
+ 
+
+    1. 5 >= 1
+    2. 0 === 1 
+    3. 4 <= 1 
+    4. 1 != 1 
+    5. "A" > "B" 
+    6. "B" < "C" 
+    7. "a" > "A" 
+    8. "b" < "A" 
+    9. true === false 
+    10. true != true
+
+
+
+ 3. Make the string: "Hi There! It's "sunny" out" by using the + sign.
+ 
+    console.log("Hi there! " + "it's sunny out");
+
+
+
+ 4. add variable "firstName" and "lastName" - so that they equal your name
+    firstName = "Zach "
+    lastName = "Rossman"
+    console.log(firstName + lastName);
+
+
+
+ 5. create a variable that holds the answer // of "firstName" + " " + "lastName"
+
+    firstName = "Zach"
+    lastName = "Rossman"
+    console.log(firstName + " " + lastName);
+
+
+ 6. What is a + b?
+
+    var a = 34;
+    var b = 21;
+    a = 2;
+    console.log(a + b);
+    55
+
+
+7. For the previous question, what is 'c' equal to?
+
+    Place your answer here
+
+ 8. Make a Calculator! using prompt(), and variables, make a program that does the following:
+ a. Prompts the user for first number.
+ b. Stores that first number
+c. Prompts the user for the second number.
+ d. stores that number and responds with the SUM by using an alert. 
+
+
+    x = parseFloat(parseInt(prompt("First number, please!")));
+    y = parseFloat(parseInt(prompt("Second number, please!")));
+    let z = x+y;
+    alert(z);
+
+
+
+
+Good Grades Exercise
+
+max = 50;
+let x = prompt("How many did you miss?");
+let y = (max-x)/max*100;
+alert(y + "%");
+
+*/
+
+
+
+Cricket Exercise
+
+x = parseInt(prompt("Chirps in fifteen seconds"));
+let y = x+40+.5;
+alert(y +' degrees Fahrenheit');
